191050f382350549fb8fa37f2583e21c5a5ee966
jiwon <kjw042626@gmail.com>
2025-05-30 16:53:34 +0900

    프로젝트 환경설정 - 프로젝트 생성 - 라이브러리 확인 - View 환경 설정 - H2데이터베이스 연결 - JPA와 DB 설정, 동작확인

---
+++ src/test/java/rejpabook/rejpashop/MemberRepositoryTest.java
@@ -0,0 +1,36 @@
+package rejpabook.rejpashop;
+
+import jakarta.transaction.Transactional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() {
+
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member); //JPA 엔티티 동일성보장
+    }
+
+}
